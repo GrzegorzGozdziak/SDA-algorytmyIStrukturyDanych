23ad32ba252021b9b3984c0ff1021f89a95eef40
unknown <ggozdziak@gmail.com>
2017-02-16 21:04:04 +0100

    Added toString and delete methods

--- src/algorytmy/structures/MyLinkedList.java
+++ src/algorytmy/structures/MyLinkedList.java
@@ -34,15 +34,23 @@ public class MyLinkedList implements MyListInterface {
 
     @Override
     public int get(int index) {
+        MyLinkedList myLinkedList = getElement(index);
+        if(myLinkedList == null) {
+            System.out.println("Wrong index");
+            return 0;
+        }
+        return myLinkedList.value;
+    }
+
+    private MyLinkedList getElement(int index){
         MyLinkedList myLinkedList = this;
         for (int i = 0; i < index; i++) {
             if(myLinkedList.next == null) {
-                System.out.println("Wrong index");
-                return 0;
+                return null;
             }
             myLinkedList = myLinkedList.next;
         }
-        return myLinkedList.value;
+        return myLinkedList;
     }
 
     @Override
@@ -62,7 +70,12 @@ public class MyLinkedList implements MyListInterface {
 
     @Override
     public void delete(int index) {
-
+        if(index < getSize()){
+            MyLinkedList previusElement = getElement(index - 1);
+            MyLinkedList deletingElement = getElement(index);
+            previusElement.next = deletingElement.next;
+            deletingElement.next = null;
+        }
     }
 
     @Override
@@ -72,6 +85,26 @@ public class MyLinkedList implements MyListInterface {
 
     @Override
     public int getSize() {
-        return 0;
+        int counter = 0;
+        MyLinkedList myLinkedList = this;
+        while(myLinkedList.next != null){
+            counter++;
+            myLinkedList = myLinkedList.next;
+        }
+        return counter;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("[");
+        for (int i = 0; i < this.getSize(); i++) {
+            stringBuilder.append(this.get(i));
+            if (i != getSize() - 1) {
+                stringBuilder.append(", ");
+            }
+        }
+        stringBuilder.append("]");
+        return stringBuilder.toString();
     }
 }
