c54cea77adff8f470cecad8c406d6bd3b03178bc
unknown <ggozdziak@gmail.com>
2017-02-18 10:53:15 +0100

    Finished linked list

--- src/algorytmy/structures/MyLinkedList.java
+++ src/algorytmy/structures/MyLinkedList.java
@@ -9,6 +9,13 @@ public class MyLinkedList implements MyListInterface {
 
     private MyLinkedList next;
 
+    public MyLinkedList() {
+    }
+
+    private MyLinkedList(int value) {
+        this.value = value;
+    }
+
 
     @Override
     public void add(int value) {
@@ -21,7 +28,7 @@ public class MyLinkedList implements MyListInterface {
 
     private MyLinkedList getLast() {
         MyLinkedList myLinkedList = this;
-        while(myLinkedList.next != null) {
+        while (myLinkedList.next != null) {
             myLinkedList = myLinkedList.next;
         }
         return myLinkedList;
@@ -29,23 +36,38 @@ public class MyLinkedList implements MyListInterface {
 
     @Override
     public void add(int index, int value) {
+        if (checkIndex(index)) {
+            MyLinkedList previousElement = getElement(index - 1);
+            MyLinkedList shiftingElement = getElement(index);
+            MyLinkedList addingElement = new MyLinkedList(value);
 
+            addingElement.next = shiftingElement;
+            previousElement.next = addingElement;
+        }
+
+    }
+
+    private boolean checkIndex(int index) {
+        return index >= 0 && index < getSize();
     }
 
     @Override
     public int get(int index) {
         MyLinkedList myLinkedList = getElement(index);
-        if(myLinkedList == null) {
+        if (myLinkedList == null) {
             System.out.println("Wrong index");
             return 0;
         }
         return myLinkedList.value;
     }
 
-    private MyLinkedList getElement(int index){
+    private MyLinkedList getElement(int index) {
         MyLinkedList myLinkedList = this;
-        for (int i = 0; i < index; i++) {
-            if(myLinkedList.next == null) {
+        if (index < -1) {
+            return null;
+        }
+        for (int i = 0; i <= index; i++) {
+            if (myLinkedList.next == null) {
                 return null;
             }
             myLinkedList = myLinkedList.next;
@@ -55,22 +77,29 @@ public class MyLinkedList implements MyListInterface {
 
     @Override
     public void put(int index, int value) {
-
+        MyLinkedList changedElement = getElement(index);
+        if (changedElement != null) {
+            changedElement.value = value;
+        }
     }
 
     @Override
     public void addAll(MyListInterface myList) {
-
+        for (int i = 0; i < myList.getSize(); i++) {
+            this.add(myList.get(i));
+        }
     }
 
     @Override
     public void addAll(int index, MyListInterface myList) {
-
+        for (int i = 0; i < myList.getSize(); i++) {
+            this.add(index + i, myList.get(i));
+        }
     }
 
     @Override
     public void delete(int index) {
-        if(index < getSize()){
+        if (index < getSize()) {
             MyLinkedList previusElement = getElement(index - 1);
             MyLinkedList deletingElement = getElement(index);
             previusElement.next = deletingElement.next;
@@ -80,14 +109,18 @@ public class MyLinkedList implements MyListInterface {
 
     @Override
     public MyListInterface clone() {
-        return null;
+        MyListInterface clonedList = new MyLinkedList();
+        for (int i = 0; i < this.getSize(); i++) {
+            clonedList.add(this.get(i));
+        }
+        return clonedList;
     }
 
     @Override
     public int getSize() {
         int counter = 0;
         MyLinkedList myLinkedList = this;
-        while(myLinkedList.next != null){
+        while (myLinkedList.next != null) {
             counter++;
             myLinkedList = myLinkedList.next;
         }
--- src/test/algorytmy/structures/MyLinkedListTests.java
+++ src/test/algorytmy/structures/MyLinkedListTests.java
@@ -21,5 +21,45 @@ public class MyLinkedListTests {
         System.out.println(myLinkedList);
         System.out.println();
 
+        System.out.println("Homework:");
+
+        System.out.println();
+
+        myLinkedList.add(1, 1);
+        System.out.println(myLinkedList);
+
+        System.out.println();
+
+        myLinkedList.put(2, 333);
+        System.out.println(myLinkedList);
+
+        System.out.println();
+
+        MyListInterface myList = new MyLinkedList();
+        myList.add(4);
+        myList.add(5);
+        myList.add(1,13);
+
+        myLinkedList.addAll(myList);
+        System.out.println(myList);
+        System.out.println(myLinkedList);
+
+        System.out.println();
+
+        myLinkedList.addAll(3, myList);
+        System.out.println(myLinkedList);
+
+//        System.out.println();
+//
+//        MyListInterface myClonedList = myLinkedList.clone();
+//        System.out.println(myClonedList);
+//        myClonedList.add(0,222);
+//        System.out.println(myClonedList);
+//
+//        System.out.println();
+
+//        myLinkedList.addAll(myClonedList);
+//        System.out.println(myLinkedList);
+
     }
 }
