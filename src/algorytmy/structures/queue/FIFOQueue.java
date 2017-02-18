4df33baa1d25703dedaac645af098ffdac7518be
unknown <ggozdziak@gmail.com>
2017-02-18 12:23:34 +0100

    Added FIFO queue methods

---
+++ src/algorytmy/structures/queue/FIFOQueue.java
@@ -0,0 +1,86 @@
+package algorytmy.structures.queue;
+
+
+
+/**
+ * Created by RENT on 2017-02-18.
+ */
+public class FIFOQueue implements Queue {
+
+    private  int value;
+
+    private FIFOQueue next;
+
+    public FIFOQueue() {
+    }
+
+    private FIFOQueue(int value) {
+        this.value = value;
+    }
+
+    @Override
+    public void push(int value) {
+        FIFOQueue last = getLast();
+        last.next = new FIFOQueue(value);
+    }
+
+    @Override
+    public int pop() {
+        if (this.next == null) {
+            System.out.println("Queue is empty");
+            return 0;
+        }
+        FIFOQueue first = this.next;
+        this.next = first.next;
+        first.next = null;
+        return first.value;
+    }
+
+    @Override
+    public Queue clone() {
+        FIFOQueue clonedQueue = new FIFOQueue();
+        FIFOQueue returnQueue = clonedQueue;
+        FIFOQueue tmpQueue = this.next;
+        while(tmpQueue != null) {
+            clonedQueue.next = new FIFOQueue(tmpQueue.value);
+            tmpQueue = tmpQueue.next;
+            clonedQueue = clonedQueue.next;
+        }
+        return returnQueue;
+    }
+
+    /*@Override
+    public MyListInterface clone() {
+        MyListInterface clonedList = new MyLinkedList();
+        for (int i = 0; i < this.getSize(); i++) {
+            clonedList.add(this.get(i));
+        }
+        return clonedList;
+    }*/
+
+    private FIFOQueue getLast() {
+        FIFOQueue myFifoQueue = this;
+        while (myFifoQueue.next != null) {
+            myFifoQueue = myFifoQueue.next;
+        }
+        return myFifoQueue;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("[");
+
+        FIFOQueue fifoQueue = this.next;
+        while(fifoQueue != null) {
+            stringBuilder.append(fifoQueue.value);
+            if(fifoQueue.next != null) {
+                stringBuilder.append(", ");
+            }
+            fifoQueue = fifoQueue.next;
+        }
+
+        stringBuilder.append("]");
+        return stringBuilder.toString();
+    }
+}
---
+++ src/test/algorytmy/structures/queue/FIFOQueueTests.java
@@ -0,0 +1,33 @@
+package test.algorytmy.structures.queue;
+
+import algorytmy.structures.queue.FIFOQueue;
+import algorytmy.structures.queue.Queue;
+
+/**
+ * Created by RENT on 2017-02-18.
+ */
+public class FIFOQueueTests {
+    public static void main(String[] args) {
+        FIFOQueue queue = new FIFOQueue();
+        queue.push(10);
+        queue.push(1);
+        queue.push(19);
+        queue.push(9);
+        System.out.println(queue);
+        queue.pop();
+        queue.pop();
+        queue.pop();
+        queue.pop();
+        System.out.println(queue);
+        queue.pop();
+        System.out.println(queue);
+        queue.push(10);
+        queue.push(1);
+        queue.push(19);
+        queue.push(9);
+        System.out.println(queue);
+
+        Queue clone = queue.clone();
+        System.out.println(clone);
+    }
+}
